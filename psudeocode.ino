3c98aa0c4cf0ba7f51e3ba080118af8985830a54
Joshua Bays <jb0401@uah.edu>
2023-10-24 17:15:34 -0500

    Implement Sensors

--- psudeocode.ino
+++ psudeocode.ino
@@ -2,8 +2,18 @@
 
 SoftwareSerial XBee(0, 1); // RX, TX
 
+Sensor sensor1, sensor2;
+
 void setup(){
+	// Communication
 	Serial.begin(9600); // Remove once finalized
 	XBee.begin(9600);
 	XBee.println("XBee connected");
+	
+	connect_sensor(sensor1);
+	connect_sensor(sensor2);
+}
+
+void loop(){
+
 }
