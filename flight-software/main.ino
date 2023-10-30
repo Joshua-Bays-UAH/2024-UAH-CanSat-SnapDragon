09401044ff1970c054615b22972b619bcd9561e6
Joshua Bays <jb0401@uah.edu>
2023-10-30 05:50:08 -0500

    update main.ino

--- flight-software/main.ino
+++ flight-software/main.ino
@@ -1,29 +1,56 @@
+/*
+Author: Joshua Bays
+Project: 2023 UAH CanSat Team Snapdragon
+*/
+
+/* 
+TODO
+	1. Create flight mode 
+	2. Create simulation mode
+	3. Create way to transfer from flight to simulation
+	4. Create command inputs
+	5. Copy this code and make sensor data real
+		1. Sensors
+		2. Pins
+*/
+
 #include <SoftwareSerial.h>
 #include <string>
-#define TeamID "SNAP" #define BuzzerPin -1
-#define LEDPin -1
+
+#define TeamID "SNAP"
+#define BuzzerPin -1 // UPDATE
+#define LEDPin -1 // UPDATE
 
 SoftwareSerial XBee(0, 1); // RX, TX
 SoftwareSerial SD(12, 11); // RX, TX
 
-#define ServoPin1 -1
-#define ServoPin2 -1
-#define ServoPin3 -1
-Sensor sensor1, sensor2;
+#define ServoPin1 -1 // UPDATE
+#define ServoPin2 -1 // UPDATE
+#define ServoPin3 -1 // UPDATE
+Sensor sensor1, sensor2; // MAKE REAL
+
+#define ReleaseHeight 760
+#define ParaHeight 100
 
 unsigned long programTime;
 unsigned baseTime;
 
-unsigned long packetct = 0;
-char mode = 'f'; char state = 0;
-float altitude; float airspeed;
-char hs = 0; char pc = 0;
-float temp; float pressure; float voltage;
-std::string gpsTime;
-float gpsAltitude;
-float gpsLatitude; float gpsLongitude;
-float tiltX; float tiltY; float rotX;
-char cmdEcho;
+unsigned long packetct = 0; // how many packets have been sent
+char mode = 'f'; // flight mode | F: FLIGHT, S:SIMULATION
+char state = 'W'; // software state | W: LAUNCH_WAIT, A: ASCENT, R: ROCKET_SEPARATION, D: DESCENT, H: HS_RELEASE, L: LANDED
+float altitude; // current altitude
+float airspeed; // current airspeed
+char hs = 0; // heat shield | P: DEPLOYED, N: NOT_DEPLOYED
+char pc = 0; // parachute | C: DEPLOYED, N: NOT_DEPLOYED
+float temp; // current temperature
+float voltage; // current voltage
+float pressure; // current air pressure
+std::string gpsTime; // current GPS time
+float gpsAltitude; // current GPS altitude
+float gpsCoords[2]; // current GPS latitude and longitude (in that order)
+unsigned gpsStats; // current count of GPS connections
+float gyro[3]; // current X and Y tilt, and Z rotation (in that order)
+char cmdEcho; // last command received
 
 std::string packet;
 
@@ -35,14 +62,14 @@ void setup(){
 	XBee.begin(9600);
 	XBee.println("XBee connected");
 	
-	connect_sensor(sensor1);
-	connect_sensor(sensor2);
+	connect_sensor(sensor1); // MAKE REAL
+	connect_sensor(sensor2); // MAKE REAL
 	
-	connect_sd(SD);
+	connect_sd(SD); // MAKE REAL
 	
-	connect_servo(servo1);
-	connect_servo(servo2);
-	connect_servo(servo3);
+	connect_servo(servo1); // MAKE REAL
+	connect_servo(servo2); // MAKE REAL
+	connect_servo(servo3); // MAKE REAL
 }
 
 void loop(){
