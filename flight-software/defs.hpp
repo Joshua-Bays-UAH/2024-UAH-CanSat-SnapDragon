4252933cd1dadb9fbda7e5d5c05e768a5e3915e6
Joshua Bays <jb0401@uah.edu>
2023-10-30 06:09:05 -0500

    renamed definitions file to match c++-style

---
+++ flight-software/defs.hpp
@@ -0,0 +1,16 @@
+#define TeamID "SNAP"
+#define BuzzerPin -1 // UPDATE
+#define LEDPin -1 // UPDATE
+
+#define XBeeRX 0
+#define XBeeTX 1
+
+#define SDRX 12
+#define SDTX 11
+
+#define ServoPin1 -1 // UPDATE
+#define ServoPin2 -1 // UPDATE
+#define ServoPin3 -1 // UPDATE
+
+#define ReleaseHeight 760 // UPDATE
+#define ParaHeight 100
