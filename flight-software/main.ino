84e7f13c89dcb4dc401875c2cb5c6d3b29db43b4
Joshua Bays <jb0401@uah.edu>
2023-11-22 10:12:46 -0600

    merged code into main.ino

--- flight-software/main.ino
+++ flight-software/main.ino
@@ -18,7 +18,38 @@ TODO
 #include <string>
 #include <string.h>
 #include <time.h>
-#include "defs.hpp"
+
+#define TeamID "2079"
+
+#define XBeeRX 0
+#define XBeeTX 1
+
+#define SDRX 9
+#define SDTX 8
+
+#define BuzzerPin 10
+#define LEDPin 11
+
+#define ReleasePin 12
+#define ParaPin 13
+
+#define BHigh digitalWrite(BuzzerPin, HIGH)
+#define BLow digitalWrite(BuzzerPin, HIGH)
+
+#define AscentHeight 5
+#define ReleaseHeight 760
+#define ParaHeight 100
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+#define LSM_CS 10
+#define LSM_SCK 13
+#define LSM_MISO 12
+#define LSM_MOSI 11
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 
 Adafruit_LSM6DSOX sox;
 Adafruit_BMP3XX bmp;
@@ -61,6 +92,159 @@ char packetBuff[128];
 
 unsigned releaseTimer;
 
+void setup(){
+	baseTime = get_baseTime();
+	
+	// Communication
+	Serial.begin(9600); // Remove once finalized
+	XBee.begin(9600);
+	XBee.println("XBee connected");
 
-#include "setup.ino"
-#include "loop.ino"
+	// SD card
+	SD.begin(9600);
+	
+	while(!sox.begin_I2C()){ delay(10); XBee.println("LSM!"); }
+	XBee.println("LSM6DSOX connected");
+	
+	XBee.print("Accelerometer range set to: ");
+	switch(sox.getAccelRange()){
+		case LSM6DS_ACCEL_RANGE_2_G: XBee.println("+-2G"); break;
+		case LSM6DS_ACCEL_RANGE_4_G: XBee.println("+-4G"); break;
+		case LSM6DS_ACCEL_RANGE_8_G: XBee.println("+-8G"); break;
+		case LSM6DS_ACCEL_RANGE_16_G: XBee.println("+-16G"); break;
+	}
+
+	XBee.print("Gyro range set to: ");
+	switch(sox.getGyroRange()){
+		case LSM6DS_GYRO_RANGE_125_DPS: XBee.println("125 degrees/s"); break;
+		case LSM6DS_GYRO_RANGE_250_DPS: XBee.println("250 degrees/s"); break;
+		case LSM6DS_GYRO_RANGE_500_DPS: XBee.println("500 degrees/s"); break;
+		case LSM6DS_GYRO_RANGE_1000_DPS: XBee.println("1000 degrees/s"); break;
+		case LSM6DS_GYRO_RANGE_2000_DPS: XBee.println("2000 degrees/s"); break;
+		case ISM330DHCX_GYRO_RANGE_4000_DPS: break; //unsupported range for the DSOX
+	}
+
+	XBee.print("Accelerometer data rate set to: ");
+	switch(sox.getAccelDataRate()){
+		case LSM6DS_RATE_SHUTDOWN: XBee.println("0 Hz"); break;
+		case LSM6DS_RATE_12_5_HZ: XBee.println("12.5 Hz"); break;
+		case LSM6DS_RATE_26_HZ: XBee.println("26 Hz"); break;
+		case LSM6DS_RATE_52_HZ: XBee.println("52 Hz"); break;
+		case LSM6DS_RATE_104_HZ: XBee.println("104 Hz"); break;
+		case LSM6DS_RATE_208_HZ: XBee.println("208 Hz"); break;
+		case LSM6DS_RATE_416_HZ: XBee.println("416 Hz"); break;
+		case LSM6DS_RATE_833_HZ: XBee.println("833 Hz"); break;
+		case LSM6DS_RATE_1_66K_HZ: XBee.println("1.66 KHz"); break;
+		case LSM6DS_RATE_3_33K_HZ: XBee.println("3.33 KHz"); break;
+		case LSM6DS_RATE_6_66K_HZ: XBee.println("6.66 KHz"); break;
+	}
+
+	XBee.print("Gyro data rate set to: ");
+	switch(sox.getGyroDataRate()){
+		case LSM6DS_RATE_SHUTDOWN: XBee.println("0 Hz"); break;
+		case LSM6DS_RATE_12_5_HZ: XBee.println("12.5 Hz"); break;
+		case LSM6DS_RATE_26_HZ: XBee.println("26 Hz"); break;
+		case LSM6DS_RATE_52_HZ: XBee.println("52 Hz"); break;
+		case LSM6DS_RATE_104_HZ: XBee.println("104 Hz"); break;
+		case LSM6DS_RATE_208_HZ: XBee.println("208 Hz"); break;
+		case LSM6DS_RATE_416_HZ: XBee.println("416 Hz"); break;
+		case LSM6DS_RATE_833_HZ: XBee.println("833 Hz"); break;
+		case LSM6DS_RATE_1_66K_HZ: XBee.println("1.66 KHz"); break;
+		case LSM6DS_RATE_3_33K_HZ: XBee.println("3.33 KHz"); break;
+		case LSM6DS_RATE_6_66K_HZ: XBee.println("6.66 KHz"); break;
+	}
+
+	XBee.println("Adafruit BMP388 / BMP390 test");
+	if(!bmp.begin_I2C()){ XBee.println("Could not find a valid BMP3 sensor, check wiring!"); }
+
+	// Set up oversampling, filter initialization, and ground elevation
+	bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+	bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+	bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+	bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+	while(!bmp.performReading()){ XBee.println("Failed to perform reading"); }
+	for(unsigned i = 0; i < 100; i++){ GroundElevation = bmp.readAltitude(SEALEVELPRESSURE_HPA); } XBee.println("Ground Elevation Set");
+
+	// Print out packet format
+	packet = "TEAM_ID,MISSION_TIME,PACKET_COUNT,SW_STATE, PL_STATE,ALTITUDE, TEMP, VOLTAGE, GPS_LATITUDE, GPS_LONGITUDE, GYRO_R,GYRO_P,GYRO_Y";
+	for(unsigned i = 0; i < packet.size(); i++){ XBee.print(packet[i]);} XBee.println();
+
+	pinMode(BuzzerPin, OUTPUT); pinMode(LEDPin, OUTPUT);
+	packetTimer = time(0);
+
+	oldAlt = 0; velocity = 0;
+}
+
+void loop(){
+	// Update how long the program has been running
+	programTime = millis();
+
+	if(mode == 'F'){
+		if(bcn){BcnCmd:
+			digitalWrite(BuzzerPin, bcnOn ? HIGH : LOW);
+			digitalWrite(LEDPin, bcnOn ? HIGH : LOW);
+			bcnOn = !bcnOn;
+		}
+	}else if(mode == 'S'){ReadPacketCmd:
+		// Read packet
+		XBee.readBytesUntil('\0', packetBuff, sizeof(packetBuff));
+		// Parse packet
+	}
+	// Make decisions
+	if(altitude >= AscentHeight && state == 'W'){AscentCmd: // Check to move into ascent
+		state = 'A';
+	}
+	if(altitude >= ReleaseHeight && state == 'A'){ReleaseCmd: // Check to move into release (and release)
+		//spin_servo(servo1);
+		state = 'R';
+	}
+	if(state == 'R'){DescentCmd: // 
+		
+	}
+	if(state == 'D'){HSCmd:
+		
+	}
+	if(state == 'H'){LandCmd:
+		
+	}
+
+	if(XBee.available){
+		XBee.readBytesUntil('\0', packetBuff, sizeof(packetBuff));
+		if(strncmp(packetBuff, "CMD,2079,", 9) == 0){
+			if(strncmp(packetBuff+9, "CX,", 3) == 0){
+				if(strncmp(packetBuff+12, "ON", 2) == 0){
+					cx = 1;
+				}else if(strncmp(packetBuff+12, "OFF", 3) == 0){
+					cx = 0;
+				}
+			}else if(strncmp(packetBuff+9, "ST", 2) == 0){
+				gpsTime = "00:00:00";
+			}else if(strncmp(packetBuff+9, "SIM", 3) == 0){
+				if(strncmp(packetBuff+13, "ENABLE", 6) == 0){
+					sEnable = 1;
+				}else if(strncmp(packetBuff+13, "ACTIVATE", 8) == 0 && sEnable){
+					state = 'S';
+				}else if(strncmp(packetBuff+13, "DISABLE", 7)){
+					state = 'F';
+					sEnable = 0;
+				}
+			}else if(strncmp(packetBuff+9, "SIMP", 4) == 0 && state == 'S'){
+				pressure = std::stof(packetBuff+10);
+			}else if(strncmp(packetBuff+9, "CAL", 3) == 0){
+				GroundElevation = altitude;
+			}else if(strncmp(packetBuff+9, "BCN", 3) == 0){
+				if(strncmp(packetBuff+12, "ON", 2){
+					bcn = 1;
+				}else if(strncmp(packetBuff+12, "OFF", 3){
+					bcn = 0;
+				}
+			}
+		}
+	}
+
+	if(cx){SendPacketCmd:
+		
+	}
+}
+
+
