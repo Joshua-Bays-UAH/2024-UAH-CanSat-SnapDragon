21278a4a7a73621c6885e5bc34e1b9df23a1d0ce
Joshua Bays <jb0401@uah.edu>
2024-03-05 21:40:22 -0600

    updated flight software

---
+++ flight-software/defs.h
@@ -0,0 +1,18 @@
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+#define CmdPrefix "CMD,2079,"
+#define CmdPreLen 9
+
+#define TEAM_ID 2079
+#define PacketSpeed 990 /* Send a packet every 1000ms */
+
+#define AscentAlt 4
+#define SeparateAltitude 8 /* Altitude to separate from rocket (Should be 700) */
+#define HRAltitutde 3 /* Altitude to release heat shield (Should be 100) */
+#define LandAlt 1 /* Altitude to determine if landed (Should be 5) */
+
+
+#define PacketLength 512
+#define CmdLength 64
+
+
--- flight-software/main.ino
+++ flight-software/main.ino
@@ -7,10 +7,8 @@
 
 #include <string.h>
 #include <utility/imumaths.h>
-#define BNO055_SAMPLERATE_DELAY_MS (100)
+#include "defs.h"
 
-#define TEAM_ID 2079
-#define PacketSpeed 990 /* Send a packet every 1000ms */
 const char Modes[2][12] = {"FLIGHT", "SIMULATION"};
 const char States[6][12] = {"LAUNCH_WAIT", "ASCENT", "SEPARATE", "DESCENT", "HS_RELEASE", "LANDED"};
 char mission_time[16] = "";
@@ -35,7 +33,8 @@ float rot_z = 0;
 char cmd_echo[64] = "";
 float velocity = 0;
 
-char packet[512];
+char packet[PacketLength];
+char cmd[CmdLength]; char c;
 
 float GroundAltitude = 0;
 float pa = 0;
@@ -43,35 +42,40 @@ float pa = 0;
 long packetTimer;
 long veloTimer;
 
-#define AscentAlt 4
-#define SeparateAltitude 8 /* Altitude to separate from rocket (Should be 700) */
-#define HRAltitutde 3 /* Altitude to release heat shield (Should be 100) */
-#define LandAlt 1 /* Altitude to determine if landed (Should be 5) */
+bool cx = 1;
+bool bcn = 1;
+bool simE = 0;
+bool simA = 0;
 
 BMP388_DEV bmp388; /* BMP 388 sensor */
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28); /* BNO055 sensor*/
 
-SoftwareSerial SD(1, 0); /* OpenLog SD Card */
-
+SoftwareSerial XBee(0, 1); // RX, TX
+SoftwareSerial SD(4, 5); /* OpenLog SD Card */
 
 void setup(void){
-	/* If Gabe's PCB is used, the following 3 lines are needed */
-	/* Wire.begin(); */
-	/* Wire.setSDA(8); */
-	/* Wire.setSCL(9); */
-
-	/* Attempt to open Serial line */
+	/* Attempt to open Serial line (testing purposes) */
 	Serial.begin(9600);
-	for(int i = 0; i < 10 && !Serial; i++){
+	while(!Serial){ delay(500); Serial.begin(9600); }
+
+	/* If Gabe's PCB is used, the following 3 lines are needed */
+	Wire.setSDA(8);
+	Wire.setSCL(9);
+	//Wire.begin();
+
+	XBee.begin(9600);
+	for(int i = 0; i < 10 && !XBee; i++){
 		delay(150);
 	}
-	Serial.println("Serial connected.");
-
+	Serial.println("XBee connected.");
+	
+	/*
 	SD.begin(9600);
 	for(int i = 0; i < 10 && !SD; i++){
 		delay(150);
 	}
-	SD.println("Serial connected.");
+	SD.println("OpenLog connected.");
+	*/
 
 	for(int i = 0; i < 10 && !bno.begin(); i++){
 		Serial.println("BNO not connected!");
@@ -131,13 +135,41 @@ void loop(void){
 		state++;
 		Serial.println(States[state]);
 	}
-
+	
+	/* Command handling */
+	if(XBee.available()){
+		c = XBee.read();
+		if(c == 13){
+			Serial.println(cmd);
+			if(strncmp(cmd, CmdPrefix, CmdPreLen) == 0){
+				if(strncmp(cmd+CmdPreLen, "CX,ON", 5) == 0){ cx = 1; }
+				else if(strncmp(cmd+CmdPreLen, "CX,OFF", 6) == 0){ cx = 0; }
+				else if(strncmp(cmd+CmdPreLen, "CAL", 3) == 0){ GroundAltitude = altitude; }
+				else if(strncmp(cmd+CmdPreLen, "PCKT,", 5) == 0){ sscanf(cmd+14, "%i", &packet_count); }
+				else if(strncmp(cmd+CmdPreLen, "ST,", 3) == 0){
+					if(strncmp(cmd+CmdPreLen, "GPS", 3) == 0){ /* Set to GPS */ }
+					else{ /* Set to manual time */ }}
+				else if(strncmp(cmd+CmdPreLen, "BCN,ON", 6) == 0){ bcn = 1; }
+				else if(strncmp(cmd+CmdPreLen, "BCN,OFF", 7) == 0){ bcn = 0; }
+				else if(strncmp(cmd+CmdPreLen, "SIM,ENABLE", 10) == 0){ simE = 1; }
+				else if(strncmp(cmd+CmdPreLen, "SIM,ACTIVATE", 12) == 0){ if(simE){ simA = 1; } }
+				else if(strncmp(cmd+CmdPreLen, "SIM,DISABLE", 11) == 0){ simE = 0; simA = 0; }
+			}
+			Serial.println(cmd);
+			for(int i = 0; i < CmdLength; i++){ cmd[i] = '\0'; }
+		}else{
+			cmd[strlen(cmd)] = c;
+		}
+	}
+	
 	/* Send telemetry */
-	if(millis() - packetTimer >= PacketSpeed){
-		velocity = (altitude - pa);
-		sprintf(packet, "%u,%f,%f,%f,%f,%f,%f, %f, %s", TEAM_ID, tilt_x, tilt_y, rot_z, temperature, pressure, altitude-GroundAltitude, velocity, States[state]);
-		/* sprintf(packet, "%u,%s,%u,%s,%s,%f,%f,%c,%c,%f,%f,%f,%s,%f,%f,%f,%u,%f,%f,%f,%s,,%f", TEAM_ID, mission_time[16], packet_count, Modes[mode], States[state], altitude, air_speed, hs_deployed, pc_deployed, temperature, voltage, pressure, gps_time[16], gps_altitude, gps_latitude, gps_longitude, gps_sats, tilt_x, tilt_y, rot_z, cmd_echo[64], velocity); */
+	if(cx && millis() - packetTimer >= PacketSpeed){
+		velocity = (altitude - pa); pa = altitude;
+		sprintf(packet, "%u,%i,%f,%f,%f,%f,%f,%f, %f, %s", TEAM_ID, packet_count, tilt_x, tilt_y, rot_z, temperature, pressure, altitude-GroundAltitude, velocity, States[state]);
+		//sprintf(packet, "%u,%s,%u,%s,%s,%f,%f,%c,%c,%f,%f,%f,%s,%f,%f,%f,%u,%f,%f,%f,%s,,%f", team_id, mission_time[16], packet_count, modes[mode], states[state], altitude, air_speed, hs_deployed, pc_deployed, temperature, voltage, pressure, gps_time, gps_altitude, gps_latitude, gps_longitude, gps_sats, tilt_x, tilt_y, rot_z, cmd_echo, velocity);
+		packet_count++;
 		Serial.println(packet);
+		XBee.println(packet);
 		packetTimer = millis();
 	}
 }
